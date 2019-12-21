d96bc2244d3c8756eaff5e91e7d75a6aa9d5a727
LukeANewton <lukeantonynewton@gmail.com>
2019-12-21 14:22:38 -0500

    commented DTOs

--- src/main/java/model/DataTransferObject.java
+++ src/main/java/model/DataTransferObject.java
@@ -1,8 +1,22 @@
 package model;
 
+/**
+ * The abstract representation for a DataTransferObject. DataTransferObjects 
+ * are used by the user interface to communicate user requests with the
+ * Controller in a way that is independent of the user interface implementation.
+ * 
+ * @author luke
+ *
+ */
 public abstract class DataTransferObject {
+	//the type of command the transfer object represents. must be set by each subclass constructor
 	protected CommandType commandType;
 	
+	/**
+	 * returns the type of command the object represents
+	 * 
+	 * @return the type of command the object represents
+	 */
 	public CommandType getCommandType() {
 		return commandType;
 	}
--- src/main/java/model/HelpDTO.java
+++ src/main/java/model/HelpDTO.java
@@ -1,17 +1,38 @@
 package model;
 
+/**
+ * A data transfer object used to issue help commands. Help commands request
+ * information about system functionalities
+ * 
+ * @author luke
+ *
+ */
 public class HelpDTO extends DataTransferObject {
+	//the type of information requested in the command
 	private HelpType helpType;
 	
+	/**Constructor*/
+	public HelpDTO() {
+		commandType = CommandType.Help;
+	}
+	
+	/**
+	 * returns the type of information requested for this help command
+	 * 
+	 * @return type of information requested for this help command
+	 */
 	public HelpType getHelpType() {
 		return helpType;
 	}
 
+	/**
+	 * set the type of information requested for this help command
+	 * 
+	 * @param helpType a member of the HelpType enumeration that denotes what information is requested
+	 */
 	public void setHelpType(HelpType helpType) {
 		this.helpType = helpType;
 	}
 
-	public HelpDTO() {
-		commandType = CommandType.Help;
-	}
+	
 }
