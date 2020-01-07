5c193f1f905bac7f3da738756086c9b1f0b7bf86
LukeANewton <lukeantonynewton@gmail.com>
2020-01-06 20:46:12 -0500

    completed FileReader to design specifications and updated Capstone driver

--- src/main/java/core/FileReader.java
+++ src/main/java/core/FileReader.java
@@ -7,51 +7,69 @@ import java.util.Scanner;
 import data_representation.DataRepresentation;
 
 /**
- * Handles the system functionalities for reading test cases from files.
+ * Handles the system functionality for reading test cases from files.
  * 
  * @author luke
 
  */
 public class FileReader {
-	//the expected format of the test case to read from file
-	private DataRepresentation parser;
-
-	/**Constructor*/
-	public FileReader() {
-		parser = null;
-	}
-
-	/**
-	 * set the format to use for reading test cases by supplying a DataRepresentation
-	 * 
-	 * @param parser the DataRepresentation object used for formatting the input
-	 */
-	public void setParser(DataRepresentation parser) {
-		this.parser = parser;
-	}
-
 	/**
 	 * reads a file into the system which contains a single test case at the specified path 
 	 * 
-	 * @param path a string representing the location of the file to read
+	 * @param filename a string representing the location of the file to read
+	 * @param format the data representation of the test case
 	 * @return the test case formatted as a DataRepresentation object
 	 * @throws FileNotFoundException thrown when no file is found at the specified path
 	 * @throws InvalidFormatException thrown when the format of the test case found does not match the expected format
 	 */
-	public String readTestCase(String path) throws FileNotFoundException, InvalidFormatException {
-		File file = new File(path); 
+	public String read(String filename, DataRepresentation format) throws FileNotFoundException, InvalidFormatException {
+		String s = readFile(filename);
+		return format.parse(s.toString());
+	}
 
-		Scanner sc = new Scanner(file); 
+	/**
+	 * reads a file's contents into a single string
+	 *
+	 * @param filename the location of the file
+	 * @return a String containing the entire contents of the file
+	 * @throws FileNotFoundException thrown when no file with the specified name is found
+	 */
+	private String readFile(String filename) throws FileNotFoundException {
+		File file = new File(filename);
+
+		Scanner sc = new Scanner(file);
 
 		StringBuffer s = new StringBuffer();
 		while (sc.hasNextLine()) {
-			s.append(sc.nextLine());  
+			s.append(sc.nextLine());
 			if(sc.hasNextLine())
 				s.append(System.lineSeparator());
 		}
 		sc.close();
-		
-		return this.parser.parse(s.toString());
+		return s.toString();
+	}
+
+	/**
+	 * reads a file into the system which contains a multiple test cases in one file at the specified path
+	 *
+	 * @param filename a string representing the location of the file to read
+	 * @param delimiter the sequence of characters used to separate test cases in a single file
+	 * @param format the data representation of the test case
+	 * @return the test case formatted as a DataRepresentation object
+	 * @throws FileNotFoundException thrown when no file is found at the specified path
+	 * @throws InvalidFormatException thrown when the format of the test case found does not match the expected format
+	 */
+	public String[] read(String filename, String delimiter, DataRepresentation format) throws FileNotFoundException, InvalidFormatException {
+		String s = readFile(filename);
+
+		//at this point, we have all the file read in, so we can split up the test cases
+		String[] testCases = s.split(delimiter);
+
+		for(int i = 0; i < testCases.length; i++){
+			testCases[i] = format.parse(testCases[i]);
+		}
+
+		return testCases;
 	}
 	
 }
