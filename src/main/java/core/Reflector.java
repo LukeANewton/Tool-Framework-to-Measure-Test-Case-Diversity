f0504169d971c6d728be940702c7d938e680b43f
BuildTools <unconfigured@null.spigotmc.org>
2020-01-07 20:17:55 -0500

    fixed args constructor for reflection component.

--- src/main/java/core/Reflector.java
+++ src/main/java/core/Reflector.java
@@ -1,12 +1,10 @@
 package core;
-import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
 
-//TODO: be able to take a file path url that leads to the class
+/**
+ * Creates instances of classes using their constructor and arguments provided by the caller.
+ */
 public class Reflector {
 
     private String classSource;
@@ -21,38 +19,45 @@ public class Reflector {
 
     /**
      * Loads a class with a no-args constructor.
+     *
      * @param className the path of the class to load relative to the root of the project.
      * @return an instance of the loaded class.
      */
     public Object loadClass(String className)
             throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         Class<?> myClass = Class.forName(classSource + className);
-        System.out.println("Successfully loaded " + myClass.getName());
         return myClass.getDeclaredConstructor().newInstance();
     }
 
     /**
-     * Loads a class with a constructor that takes one Object argument.
-     * TODO: Use varargs instead of Object[] and destructure the args when calling a constructor like what can be done
-     *          in javascript ES6. Java doesn't currently support this.
+     * Loads a class with a constructor that takes arguments.
+     * Example inputs: when classSource is "java.awt.":
+     * ("Rectangle", new Class[] {int.class, int.class}, new Object[] {5, 10})
+     *
      * @param className the path of the class to load relative to the root of the project.
-     * @param constructorArgs an array of parameters that a loaded class may need passed into its constructor.
-     * @return an instance of the loaded class.
+     * @param initArgsClasses the classes array that the constructor takes.
+     * @param constructorArgs an array of object parameters that a loaded class may need passed into it's constructor.
+     * @return an instance of the loaded class to be type casted by the caller.
      */
-    public Object loadClass(String className, Object[] constructorArgs)
+    public Object loadClass(String className, Class[] initArgsClasses, Object[] constructorArgs)
             throws NoSuchMethodException, ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        Class<?> myClass = Class.forName(classSource + className);
-        Constructor<?> constructor = myClass.getConstructor(Object.class);
-        return constructor.newInstance(constructorArgs);
+
+        Class<?> definition;
+        Constructor<?> initArgsConstructor;
+        definition = Class.forName(classSource + className);
+        initArgsConstructor = definition.getConstructor(initArgsClasses);
+        return initArgsConstructor.newInstance(constructorArgs);
     }
 
     /**
-     * Ensures that the source ends with a period for proper formatting the path
+     * Ensures that the source ends with a period for properly formatting the path.
+     * For example, src/main/java/ 'my_package.my_other_package.class_name' is the path with the quoted part the full path used.
+     *
      * @param source the package source string delimited with periods
      * @return the new source string
      */
     private String endsWithPeriod(String source) {
-        if ((source.substring(source.length() - 1)).equals(".")) {
+        if (!(source.substring(source.length() - 1)).equals(".")) {
             source = source + ".";
         }
         return source;
--- src/test/java/core/ReflectorTest.java
+++ src/test/java/core/ReflectorTest.java
@@ -1,26 +1,21 @@
 package core;
 
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-
+import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
+import java.util.Arrays;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-
+/**
+ * A test suite for the Reflection service
+ */
 public class ReflectorTest {
-
-    private static Reflector reflector;
-
-    @Before
-    public void setup() {
-        reflector = new Reflector();
-    }
 
     @Test
     public void testReflectNoArgsConstructor() {
+        Reflector reflector = new Reflector();
         reflector.setClassSource("metrics.comparison.");
         Object instance = null;
         String className = "CommonElements";
@@ -34,10 +29,60 @@ public class ReflectorTest {
         } catch (InstantiationException e) {
             System.err.println("Failed to create instance of " + className);
         } catch (NoSuchMethodException e) {
+            System.err.println("Failed to find no args constructor for class " + className);
             e.printStackTrace();
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
         assertNotNull("Failed to instantiate class '" + className + "'.", instance);
+    }
+
+    @Test
+    public void testReflectArgsConstructor() {
+        Reflector reflector = new Reflector("java.awt.");
+
+        Object instance = null;
+        String className = "Rectangle";
+        Class[] constructorTemplate = new Class[] {int.class, int.class};
+        Object[] constructorValues = new Object[] {5, 10};
+
+        try {
+            instance = reflector.loadClass(className, constructorTemplate, constructorValues);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Failed to find class " + reflector.getClassSource() + className);
+        } catch (IllegalAccessException e) {
+            System.err.println("Restricted access to file; cannot load " + className);
+        } catch (InstantiationException e) {
+            System.err.println("Failed to create instance of " + className);
+        } catch (NoSuchMethodException e) {
+            System.err.println("Failed to find args constructor for class '" + className + "' with arg template '" +
+                    Arrays.toString(constructorTemplate) + "' and args '" + Arrays.toString(constructorValues) + "'.");
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        assertNotNull("Failed to instantiate class '" + className + "'.", instance);
+
+        Rectangle rec = (Rectangle) instance;
+
+        assertEquals("Failed to set constructor argument.", 10, rec.height);
+    }
+
+    @Test
+    public void testGetClassSource() {
+        Reflector reflector = new Reflector("metrics.comparison.");
+        assertEquals("Class source obtained from reflector is incorrect.", "metrics.comparison.", reflector.getClassSource());
+    }
+
+    @Test
+    public void testEndsWithPeriodAlreadyThere() {
+        Reflector reflector = new Reflector("metrics.comparison.");
+        assertEquals("Class source obtained from reflector is incorrect.", "metrics.comparison.", reflector.getClassSource());
+    }
+
+    @Test
+    public void testEndsWithPeriodNotThere() {
+        Reflector reflector = new Reflector("metrics.comparison");
+        assertEquals("Class source obtained from reflector is incorrect.", "metrics.comparison.", reflector.getClassSource());
     }
 }
