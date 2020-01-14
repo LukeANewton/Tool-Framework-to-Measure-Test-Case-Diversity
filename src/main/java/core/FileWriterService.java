abc5f9c3c9041b5720ad17baa2fef47a77448316
Eric <ericbedard@cmail.carleton.ca>
2020-01-13 20:12:02 -0500

    Update FileWriterService.java

--- src/main/java/core/FileWriterService.java
+++ src/main/java/core/FileWriterService.java
@@ -3,29 +3,41 @@ package core;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Objects;
 
+/**
+ * The FileWriterService will write the result of the comparisons to a file .
+ * It can create a file, write to an existing file or overwrite an already existing file.
+ *
+ * @author Eric
+ */
 public class FileWriterService {
 	private String path;
 	
 	public FileWriterService(String path) {
 		this.path = path;
 	}
-	
-	public void write(String text, boolean overwrite, boolean append) throws IOException {
-		
+
+	/**
+	 * This method will write the given string to the file with the given name at the path location.
+	 * It can create a new file, overwrite an existing file or append to an already existing file.
+	 *
+	 * @param name the name of the file
+	 * @param text the text to be written to the file
+	 * @param overwrite boolean deciding if the FileWriterService should overwrite a file with the same name
+	 * @param append boolean deciding if the text should be appended to a present file
+	 * @throws IOException
+	 */
+	public void write(String name, String text, boolean overwrite, boolean append) throws IOException {
 		if(overwrite) {
-			File oldFile = new File(path + "/" + "SimilarityResult.txt");
+			File oldFile = new File(path + name);
 			oldFile.delete();
-		} 
-			
-		File file = new File(path + "/" + "SimilarityResult.txt");
-		file.createNewFile();
-		FileWriter writer;
-		if (append) {
-			writer = new FileWriter(file, true);
-		} else {
-			writer = new FileWriter(file, false);
 		}
+		File file = new File(path + name);
+		if(!append) {
+			file.createNewFile();
+		}
+		FileWriter writer = new FileWriter(file, append);
 		writer.write(text);
 		writer.write(System.lineSeparator());
 		writer.close();
