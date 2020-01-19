dd2f7ad91d779e6dd3277b5ce32181a95b5b5a98
cameronrushton <cameron.rushton@rogers.com>
2020-01-19 01:44:52 -0500

    removed commented preferences code in main

--- src/main/java/ComparisonApp.java
+++ src/main/java/ComparisonApp.java
@@ -1,35 +1,12 @@
 import core.Controller;
 
-import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 public class ComparisonApp {
 
     private void run(String[] args) {
-        boolean exist = false;
         final String APP_NAME = this.getClass().getName();
         Preferences prefs = Preferences.userRoot().node(APP_NAME);
-
-        try {
-            exist = Preferences.userRoot().nodeExists(APP_NAME);
-        } catch (BackingStoreException e) {
-            System.err.println("Failed to contact storage of OS's preference manager. User preferences will be not be used.");
-        }
-        if (!exist) {
-//            prefs.put(Settings.COMPARISON_METHOD.name(), "CommonElements");
-//            prefs.put(Settings.COMPARISON_METHOD_LOCATION.name(), "metrics.comparison.");
-//            prefs.put(Settings.DATA_REPRESENTATION.name(), "CSV");
-//            prefs.put(Settings.DATA_REPRESENTATION_LOCATION.name(), "data_representation.");
-//            prefs.put(Settings.DELIMITER.name(), "\n");
-//            prefs.put(Settings.AGGREGATION_METHOD.name(), "AverageValue");
-//            prefs.put(Settings.AGGREGATION_METHOD_LOCATION.name(), "metrics.aggregation.");
-//            prefs.put(Settings.THREAD_COUNT.name(), "3");
-//            prefs.put(Settings.RESULT_ROUNDING_SCALE.name(), "2");
-//            prefs.put(Settings.RESULT_ROUNDING_MODE.name(), "HALF_UP");
-//            prefs.put(Settings.OUTPUT_FILE_NAME.name(), "comparison_result");
-//            prefs.put(Settings.OUTPUT_FILE_LOCATION.name(), "/");
-            System.out.println("Preferences created.");
-        }
 
         Controller controller = new Controller(prefs);
         /*in order to reuse the code from the Console, for now args must be
